6c3c1063ff2f12b9bd9b56b044211c7ec955fec5
Danielrpelaez <72061176+Danielrpelaez@users.noreply.github.com>
2020-09-29 23:16:25 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,6 +11,6 @@ export const environment = {
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * on performance if an error is thrown..
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
